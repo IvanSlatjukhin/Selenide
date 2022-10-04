b609221f777866c54041b8dfa56a4b31b1061298
IvanSlatjukhin <slatjukhin@mail.ru>
2022-10-04 22:31:33 +0300

    Test

--- src/test/java/ru/netology/domain/CardDeliveryTest.java
+++ src/test/java/ru/netology/domain/CardDeliveryTest.java
@@ -29,4 +29,25 @@ public class CardDeliveryTest {
                 .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + verificationDate),
                         Duration.ofSeconds(15));                        //Загрузка не более 15 секунд
     }
+    @Test   //№2
+    public void shouldSuccessfulFormSubmissionAfterInteractingWithComplexElements() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Ан");
+        $(Selectors.byText("Анадырь")).click();
+        $("[data-test-id=date] input").click();
+        int days = 4;                                                   //количество дней после даты по умолчанию
+        for ( int cycle = 0; cycle < days; cycle++) {
+            $(".calendar").sendKeys(Keys.ARROW_RIGHT);
+        }
+        $(".calendar").sendKeys(Keys.ENTER);
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+79997778899");
+        $("[data-test-id=agreement]").click();
+        $(".button").shouldHave(Condition.text("Забронировать")).click();
+        String verificationDate = LocalDate.now().plusDays(7)           //Текущая дата плюс 7 дней
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));     //Формат даты день.месяц.год
+        $("[data-test-id=notification]")
+                .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + verificationDate),
+                        Duration.ofSeconds(15));                        //Загрузка не более 15 секунд
+    }
 }
